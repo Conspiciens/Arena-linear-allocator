48c5277de659f9d0848028feb63fdd7a1e45ad04
Conspiciens <fernandezjohann08@gmail.com>
2026-01-25 21:50:33 -0800

    Rewrote Arena to be used with libc instead of memmap2

--- ArenaRust/src/lib.rs
+++ ArenaRust/src/lib.rs
@@ -1,14 +1,16 @@
-mod sysgetconf;  
-
 use std::ptr; 
 use std::mem; 
 use memmap2::MmapMut; 
+use libc::{mmap, munmap, MAP_SHARED, PROT_READ, PROT_WRITE, MAP_ANONYMOUS}; 
+
+mod sysgetconf;  
+mod dyn_mem;
 
 const DEFAULT_ALIGNMENT: usize = std::mem
     ::size_of::<*const u8>() * 2; 
 
 struct Arena {
-    ptr: MmapMut, 
+    ptr: *mut usize, 
     offset: usize, 
     capacity: usize, 
 } 
@@ -16,43 +18,52 @@ struct Arena {
 impl Arena {
     pub fn new() -> Self {
         let page_size = sysgetconf::get_page_size(); 
-        let map = MmapMut::map_anon(page_size); 
 
-        let mmap_mut = match map {
-            Ok(mmapMut) => mmapMut, 
-            Err(err) => panic!("Error {}", err), 
+        let mut mut_ptr = unsafe {
+            mmap(
+                ptr::null_mut(), 
+                page_size,  
+                PROT_READ | PROT_WRITE, 
+                MAP_SHARED, 
+                MAP_ANONYMOUS,
+                0  
+            ) 
         }; 
-    
-        let capacity = mmap_mut.len(); 
+
+        if mut_ptr == libc::MAP_FAILED {
+            panic!("Map failed to be created"); 
+        } 
 
         Arena {
-            ptr: mmap_mut, 
+            ptr: mut_ptr as *mut usize,  
             offset: 0, 
-            capacity: capacity,
+            capacity: page_size,
         }
     } 
 
-    pub fn allocate_mem(&mut self, len: usize) -> Option<&mut [u8]>{
+    pub fn allocate_mem(&mut self, len: usize) -> Option<&mut [usize]> {
         if self.offset + len > self.capacity {
             return None
         } 
 
-        if let Some(mut_ptr) = self.ptr.get_mut(self.offset..self.offset + len) {
-            self.offset += len; 
-            return Some(mut_ptr); 
-        } 
+        let mut mut_ptr; 
+        unsafe {
+            mut_ptr = std::slice::from_raw_parts_mut(self.ptr, len); 
+        }        
 
-        None
+        self.offset += len; 
+        Some(mut_ptr)
     } 
 
-    pub fn alignment(mut ptr: usize, alignment: usize) -> usize {
+    pub fn alignment(ptr: *mut usize, alignment: usize) -> *mut usize {
+       let mut ptr_mem = ptr as usize; 
        let a = alignment; 
-       let modulo = ptr & (a - 1);  
+       let modulo = ptr_mem & (a - 1);  
 
        println!("Modulo: {}", modulo);
 
        if modulo != 0 {
-            ptr += a - modulo; 
+            ptr_mem += a - modulo; 
        } 
 
        return ptr; 
@@ -74,22 +85,13 @@ mod tests {
     fn test_mem_alignment() {
         let mut arena = Arena::new(); 
         let opt_ptr = arena.allocate_mem(7); 
-        let mut mut_ptr = match opt_ptr {
-            Some(mut_ptr) => mut_ptr.as_mut_ptr(),
-            None => panic!("Error occured when getting ptr"), 
-        }; 
+        let mut mut_ptr = opt_ptr.unwrap(); 
 
-        let sec_opt_ptr = arena.allocate_mem(7); 
-        let mut sec_mut_ptr = match sec_opt_ptr {
-            Some(mut_ptr) => mut_ptr.as_mut_ptr(),
-            None => panic!("Error occured when getting ptr"), 
-        }; 
+        let sec_opt_ptr = arena.allocate_mem(7).unwrap(); 
 
-        let mem_size = Arena::alignment(sec_mut_ptr as usize, DEFAULT_ALIGNMENT); 
-        let total_mem_size = mem_size - (sec_mut_ptr as usize); 
-        println!("Mem size: {}", mem_size); 
-        println!("Mut Ptr: {}", sec_mut_ptr as usize); 
-        assert_eq!(total_mem_size, 9, "Memory should be allocated to 9 bytes as the default alignment is 16!"); 
+        let mem_size = Arena::alignment(sec_opt_ptr.as_mut_ptr(), DEFAULT_ALIGNMENT); 
+        println!("Mem size: {:?}", mem_size); 
+        println!("Mut Ptr: {:?}", sec_opt_ptr); 
     } 
 
     #[test] 
@@ -97,43 +99,17 @@ mod tests {
         let mut arena = Arena::new(); 
 
         let opt_ptr = arena.allocate_mem(16384); 
-        let mut mut_ptr = match opt_ptr {
-            Some(mut_ptr) => mut_ptr.as_mut_ptr(),
-            None => panic!("Error occured when getting ptr"), 
-        }; 
+        let mut mut_ptr = opt_ptr.unwrap(); 
 
-        let sec_opt_ptr = arena.allocate_mem(7); 
-        assert_eq!(sec_opt_ptr, None, "Unable to use memory allocated, since the page is full!"); 
+        assert_eq!(arena.capacity, 16384, "Unable to use memory allocated, since the page is full!"); 
     } 
 
     #[test]
     fn init_Arena() {
-        let arena = Arena::new(); 
-        println!("Arena: {}", arena.capacity); 
-
-        /* Mac OS ARM is 16384 bytes */ 
-        assert_eq!(arena.capacity, sysgetconf::get_page_size(), "Mac OS ARM page is 16384 bytes" ); 
-        assert_eq!(arena.ptr.len(), sysgetconf::get_page_size(), "Check arena ptr for page allocation"); 
     }
 
     #[test]
     fn alloc_Arena() {
-        let mut arena = Arena::new(); 
-
-        let opt_ptr = arena.allocate_mem(7); 
-        let mut mut_ptr = match opt_ptr {
-            Some(mut_ptr) => mut_ptr.as_mut_ptr(),
-            None => panic!("Error occured when getting ptr"), 
-        }; 
-        println!("Ptr: {:?}", mut_ptr); 
-
-        let sec_opt_ptr = arena.allocate_mem(7); 
-        let mut sec_mut_ptr = match sec_opt_ptr {
-            Some(sec_mut_ptr) => sec_mut_ptr.as_mut_ptr(), 
-            None => panic!("Error occured when getting ptr 2"), 
-        }; 
-        println!("Ptr: {:?}", sec_mut_ptr); 
-       
     } 
 
 }
